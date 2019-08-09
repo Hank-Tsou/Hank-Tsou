4063bdddcdcd7f5c08a43c7ba8cfb7274b414559
Hank-Tsou <hank630280888@gmail.com>
2019-08-09 20:09:02 +0800

    full web

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,1 +1,17 @@
-// alert("with JavaScript");
+// $(window).scroll(function(){
+//   cityScroll();
+// });
+
+function cityScroll(){
+  // var wScroll = $(window).scrollTop();
+  // console.log(wScroll);
+  var elmnt = document.getElementById("Middle");
+  var y = elmnt.scrollTop;
+  console.log(y);
+  $('.city_img').css('background-position', 'center '+ (y-1900) +'px');
+  // if(y == 1580){
+  //   $('.city_img').css('background-position', 'center '+ (y-1580) +'px');
+  // }
+
+
+}
